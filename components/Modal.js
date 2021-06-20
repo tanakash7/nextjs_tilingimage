3f6fee62b1eeaf7f5d5edf471910894d71ac6184
tanaka7 <siosiosolid@gmail.com>
2021-06-20 17:29:20 +0900

    モーダル調整

--- components/Modal.js
+++ components/Modal.js
@@ -2,28 +2,40 @@ import { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 import styles from "../styles/Modal.module.scss"
 
-export const Modal = ({targetId, photos, handleClick}) => {
+export const Modal = ({ targetId, photos, handleClick }) => {
   console.log("Modal")
   const [photo, setPhoto] = useState(null)
-  useEffect(()=>{
-    const target = photos.filter(data=>{
+  useEffect(() => {
+    const target = photos.filter(data => {
       return data.id === targetId
     })
     setPhoto(target[0])
-  },[targetId])
+  }, [targetId])
   const stopPropagation = e => {
     e.stopPropagation()
   }
+  console.log(photo)
+  const photoURL = (() => {
+    if (!photo) return null
+    return window.innerWidth >= 768 ? photo.urls.regular : photo.urls.small
+  })()
   const element = (
     <>
       <div className={styles.Modal__background}></div>
       <div className={styles.Modal__inner} onClick={handleClick}>
         <div className={styles.Modal__contents} onClick={stopPropagation}>
-          {photo&&(
+          {photo && (
             <>
-              <div>{photo.alt_description}</div>
-              <div><img src={photo.urls.regular} alt=""/></div>
-              {/* flexで囲って右側にリンク（spは縦に並べる） */}
+              <div className={styles.Modal__img}><img src={photoURL} alt={photoURL} /></div>
+              <div className={styles.Modal__desc}>{photo.description}</div>
+              <div className={styles.Modal__download}>
+                <a
+                  href={photo.links.html}
+                  target="_blank"
+                  className={styles.Modal__downloadButton}>
+                  <span className={styles.Modal__downloadButtonText}>DOWNLOAD</span><i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
             </>
           )}
         </div>
--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,25 @@ import { Hero } from '../components/Hero'
 import { Masonry } from '../components/Masonry'
 import styles from '../styles/Home.module.scss'
 import { unsplashInstance } from '../util/axios_unsplash'
+import { config, dom, library } from '@fortawesome/fontawesome-svg-core';
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+library.add(faExternalLinkAlt)
 
-export const getServerSideProps = async() => {
+dom.watch()
+
+export const getServerSideProps = async () => {
   const data = await unsplashInstance("/photos/random?count=16").then(res => res.data)
-  return {props: {data}}
+  return { props: { data } }
 }
 
-export default function Home({data}) {
+export default function Home({ data }) {
   return (
     <>
-    <div className={styles.container}>
-      <Head>
-        <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
-      </Head>
-      <Hero />
-      <Masonry photos={data}/>
-    </div>
-    <div id="Modal"></div>
+      <div className={styles.container}>
+        <Hero />
+        <Masonry photos={data} />
+      </div>
+      <div id="Modal"></div>
     </>
   )
 }
